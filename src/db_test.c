edb5de107f6219039c0a70f5e796060d6467ac66
Kalvin <kalvin@ubuntu.ubuntu-domain>
2010-06-11 20:00:39 -0700

    Added a tester for parallel db

--- src/db_test.c
+++ src/db_test.c
@@ -1,27 +1,67 @@
 #include <stdlib.h>
-#include "db.h"
-#include <stdint.h>
-int main(void)
-{
+#include <stdio.h>
 
-  //Open a database for writing
-  void *gdbm = db_open("testdb", 1);
-  uint32_t test;
-uint32_t *values;
-uint32_t key = 23;
-uint32_t value=45;
-uint32_t value2=245;
-int i;
-  db_insert(gdbm, key, value);
-  db_insert(gdbm, key, value2);
-  db_insert(gdbm, key, value2);
-  db_insert(gdbm, key, value2);
-  db_close(gdbm);
-  gdbm = db_open("testdb", 0);
-int32_t num_values = db_query (gdbm, key, &values);
-for (i = 0; i < num_values; i++)
- printf("%u\n", values[i]);
- // if(test!=NULL)
-  db_close(gdbm);
-  return 0;
+#include <cldb.h>
+#include <pthread.h>
+
+struct arg_data{
+  struct cldb *db;
+  uint64_t key;
+  uint32_t* values;
+};  	
+
+uint64_t total=0;
+pthread_mutex_t total_lock;
+
+void prepare(void* threadarg)
+{
+   struct arg_data *my_data;
+   my_data = (struct arg_data *) threadarg;
+   int32_t gi_count = cldb_get(my_data->db, my_data->key, &(my_data->values));
+   if (gi_count < 0)
+   {
+    fprintf (stderr, "invalid count: %d\n", gi_count);
+    exit (EXIT_FAILURE);
+   }  
+    total+=gi_count;
 }
+
+int
+main (void)
+{
+  total=0;
+  pthread_mutex_init(&total_lock, NULL);
+  uint64_t key_max = 4294967296;
+  uint64_t key;
+  int i;
+  struct cldb *db = cldb_open ("/mnt/gi.bin", 4);
+  pthread_t pthreads[4];
+  if (NULL == db)
+    {
+      fprintf (stderr, "cldb_open\n");
+      exit (EXIT_FAILURE);
+    }
+
+  while(key < key_max)
+  {
+    for(i=0; i< 4; i++){
+      uint32_t *gi_values;
+      struct arg_data data;
+      data.db = db;
+      data.key = key;
+      data.values = gi_values;
+
+      pthread_create(&pthreads[i], NULL, prepare, (void*) &data);
+      key++;
+    }
+    for(i=0; i< 4; i++){
+      pthread_join(pthreads[i], NULL);
+    }
+
+  }
+   
+
+  cldb_close (db);
+  exit (EXIT_SUCCESS);
+}
+
